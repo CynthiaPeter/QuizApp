ccecd2f00defb977b8bd71271a0b02e5035aac1a
Cynthia Peter <cynthiapeterpearl@gmail.com>
2019-11-23 03:07:24 +0100

    Worked on the interface and implemented mapping and spread operrator

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,15 @@ import 'package:flutter/material.dart';
 import './questions.dart';
 import './answers.dart';
 
-
 void main() {
   runApp(QuizApp());
 }
 
 class QuizApp extends StatefulWidget {
   @override
- State<StatefulWidget> createState() {
-   return _QuizAppState();
- }
+  State<StatefulWidget> createState() {
+    return _QuizAppState();
+  }
 }
 
 class _QuizAppState extends State<QuizApp> {
@@ -24,31 +23,41 @@ class _QuizAppState extends State<QuizApp> {
     });
     print(_questionIndex);
   }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your Favorite color?',
-      'What\'s your favourite movie?',
+      {
+        'questionText': 'What\'s your Favorite color?',
+        'answers': ['Black', 'Red', 'Purple', 'White'],
+      },
+      {
+        'questionText': 'What\'s your Favorite food?',
+        'answers': ['Jollof', 'Fried Rice', 'Banga Rice', 'Coconut Rice'],
+      },
+      {
+        'questionText': 'What\'s your Favorite pet?',
+        'answers': ['Puppy', 'Snake', 'Cat', 'Rabbit'],
+      },
     ];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-      appBar: AppBar(
-        title: Text("QuizApp"),
-        centerTitle: true,
+        appBar: AppBar(
+          title: Text("QuizApp"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex]['questionText'],
+            ),
+           ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          Question(
-            questions[_questionIndex],
-          ),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion), 
-
-        ],
-      ),
-    ),
     );
   }
 }
